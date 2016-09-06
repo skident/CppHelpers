4e14ca72882fd9982238cd4d77711fbf43ea9ba0
Skident <bagriy.volodymyr@gmail.com>
2016-09-06 16:13:30 +0300

    Update stl_helper.h
    Added functions ltrim, rtrim, trim

--- stl_helper.h
+++ stl_helper.h
@@ -19,6 +19,7 @@
 #include <iostream>
 #include <sstream>
 #include <map>
+#include <cctype>
 
 #ifdef MODERN_CPP_AVAILABLE
 	#include <unordered_map>
@@ -132,4 +133,50 @@ namespace stl_helper
 
 #endif
 
+	//! Removes all whitespaces before data in string.
+	//! \param str string with data
+	//! \return string without whitespaces in left part
+	std::string ltrim(const std::string& str)
+	{
+		std::string result;
+		std::size_t len = str.length();
+		std::size_t i = 0;
+		for ( ; i < len; i++)
+		{
+			if (std::isspace(str[i]) == 0)
+				break;
+		}
+
+		if (i < len)
+			result = str.substr(i);
+		return result;
+	}
+
+	//! Removes all whitespaces after data in string
+	//! \param str string with data
+	//! \return string without whitespaces in right part
+	std::string rtrim(const std::string& str)
+	{
+		std::string result;
+		std::size_t len = str.length();
+		std::size_t i = len-1;
+		for ( ; i >= 0; i--)
+		{
+			if (std::isspace(str[i]) == 0)
+				break;
+		}
+
+		result = str.substr(0, i+1);
+		return result;
+	}
+
+	//! Removes all whitespaces before and after data in string
+	//! \param str string with data
+	//! \return string without whitespaces before and after data in string
+	std::string trim(const std::string& str)
+	{
+		std::string result = ltrim(str);
+		result = rtrim(result);
+		return result;
+	}
 };
