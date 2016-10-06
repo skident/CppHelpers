09b34d1e6683ee02e610c67d23ee9ad8829b855f
Skident <bagriy.volodymyr@gmail.com>
2016-10-06 22:13:02 +0300

    Added generic class Range for fast searching value in ranges

---
+++ range_test.cpp
@@ -0,0 +1,25 @@
+#include "Range.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Range<int> range;
+
+	range.insert({1, 3});
+	range.insert({1, 7});
+	range.insert({8, 11});
+
+	range.print();
+
+	while (true)
+	{
+		int value;
+		cin >> value;
+		auto isFind = range.find(value);
+		cout << "found state: " << boolalpha << isFind << endl;
+	}
+
+	return 0;
+}
