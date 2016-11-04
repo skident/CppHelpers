f84cee86a8dd2b1da0c9999a8a74f8224cdab10a
Skident <skident@mail.ru>
2016-11-04 18:45:00 +0200

    -del: Family of Tests classes and usage of them  -del: .sh specified for Linux.

--- Samples/RunAllTests.cpp
+++ Samples/RunAllTests.cpp
@@ -5,30 +5,14 @@
 #include "sample_wire.hpp"
 #include "sample_worker.hpp"
 
-#include <eos/tests/tests.hpp>
-
 using namespace std;
-using namespace eos;
 
-
-TEST(test, ints)
-{
-    EQ_TEST(1,4)
-}
-
-TEST(test, strings)
-{
-	EQ_TEST(string("123"), "123")
-}
 
 int main()
 {
-	TestContainer::RunAllTests();
-    
-    /*
 	run_output_samples();
 	run_range_samples();
     run_wire_samples();
-    sample_worker_usage();
-     */
+    //sample_worker_usage();
+
 }
--- buildLinux.sh
+++
@@ -1,5 +0,0 @@
-rm -rf built /y
-mkdir built
-cd built
-pwd
-cmake ../ && make
