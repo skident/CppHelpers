0c6bdd26474dd14c9f52f3c9371eeb910cd16819
Skident <skident@mail.ru>
2016-11-04 17:32:38 +0200

    - add: gtest downloading by CMake

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,35 +1,19 @@
 #include <iostream>
 
 #include <eos/types/wire.hpp>
-#include "CppUnitLite/TestHarness.h"
+#include <gtest/gtest.h>
 
 using namespace std;
+using namespace eos;
 
-SimpleString StringFrom(const std::string& value)
-{
-	return SimpleString(value.c_str());
+GTEST_API_ int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
 
-int main()
+TEST(Test, First)
 {
-	TestResult tr;
-	TestRegistry::runAllTests(tr);
-
-	return 0;
+	EXPECT_EQ(wire(1), wire(1));
 }
 
-
-TEST(Stack, creation)
-{
-	/*std::string s;
-	LONGS_EQUAL(0, s.size());
-	std::string b = "asa";
-	CHECK_EQUAL(string("asa"), b);*/
-}
-
-
-//TEST(ValidatorRegex, ManualPositive)
-//{
-//	//EXPECT_EQ(true, 1, 1);
-//}
